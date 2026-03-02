c9df197dd21259b8771cc3dfa609ce4b529539ba
KillRmnv <memes20006@gmail.com>
2026-03-02 18:05:19 +0300

    update ubuntu scripts

--- ubuntu.sh
+++ ubuntu.sh
@@ -78,8 +78,10 @@ pip install -U "huggingface_hub[cli]"
 
 curl -fsSL https://opencode.ai/install | bash
 
+#qwen code
 curl -fsSL https://qwen-code-assets.oss-cn-hangzhou.aliyuncs.com/installation/install-qwen.sh | bash
 
+#mistral vibe
 curl -LsSf https://mistral.ai/vibe/install.sh | bash
 
 # OpenBLAS
@@ -96,19 +98,80 @@ source $HOME/.cargo/env
 
 rustup update
 
+#llmfit
 git clone https://github.com/AlexsJones/llmfit.git
 cd llmfit
 cargo build --release
 # binary is at target/release/llmfit
 
+
+#models
 git clone https://github.com/arimxyer/models
 cd models
 cargo build --release
 ./target/release/models
 
-
+#Pi agent
 npm install -g @mariozechner/pi-coding-agent
 
+#Goose
 curl -fsSL https://github.com/block/goose/releases/download/stable/download_cli.sh | bash
 
+#Cline
 npm install -g cline
+
+#also install Lm Studio via Browser https://lmstudio.ai/
+
+#btop https://github.com/aristocratos/btop
+
+#tree
+sudo apt install tree
+
+
+#superfile
+bash -c "$(curl -sLo- https://superfile.dev/install.sh)"
+
+#ripgrep
+ sudo apt-get install ripgrep
+ 
+ #fd
+ sudo apt install fd-find
+ 
+ #GH
+ (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+	
+#pass	
+ sudo apt-get install pass
+ 
+ 
+ 
+ 
+#jq or go to github page and check for latest
+mkdir -p ~/bin
+wget https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-linux-amd64 -O ~/bin/jq
+chmod +x ~/bin/jq
+~/bin/jq --version
+# Ожидаемый вывод: jq-1.8.1
+# Проверьте, есть ли ~/bin уже в PATH
+echo $PATH | grep -o "$HOME/bin"
+
+# Если нет — добавьте в ~/.bashrc или ~/.zshrc
+echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
+
+# Примените изменения
+source ~/.bashrc
+
+
+#fzf
+sudo apt install fzf
+
+#tldr
+pipx install tldr
